28b0dcbe60e2b168df993e60acb30e14f5c4c034
Dione Nascimento <morrone.dione@gmail.com>
2025-10-21 21:13:59 -0300

    Update main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# Importando as bibliotecas necessárias
-import pandas as pd
-import matplotlib.pyplot as plt
-
-
-df = pd.read_csv('arquivos_originais/relacao_202401.csv',sep=';', encoding='UTF-8')
-
-print(df.columns)
-#print(df.head())
